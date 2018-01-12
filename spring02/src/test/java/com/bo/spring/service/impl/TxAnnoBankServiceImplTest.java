3a1fb4f5057e9477fd9936cd65d0ffdd2116fd07
wangboc <wangboc@bj.xinwei.com.cn>
2018-01-12 13:40:46 +0800

    增加注解事务测试用例:模拟controller层在try..catch..调用service层时的事务处理

--- spring02/src/test/java/com/bo/spring/service/impl/TxAnnoBankServiceImplTest.java
+++ spring02/src/test/java/com/bo/spring/service/impl/TxAnnoBankServiceImplTest.java
@@ -8,7 +8,9 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -21,7 +23,7 @@ import com.bo.spring.service.UserService;
  * @author 王博
  * @version 2017年9月8日　下午10:40:11
  */
-public class TxAnnoBankServiceImpl {
+public class TxAnnoBankServiceImplTest {
 
 	private static ApplicationContext ctx;
 	
@@ -58,13 +60,31 @@ public class TxAnnoBankServiceImpl {
 	public void tearDown() throws Exception {
 	}
 
+	@Ignore
 	@Test
 	public void test() throws Exception {
 		UserService userService = (UserService) ctx.getBean("userService");
 		User u1 = new User(1,"bo","1111",new Account(1,100));
 		User u2 = new User(2,"lin","1809", new Account(2,100));
-		boolean result2 = userService.NonTransactionUpdate(u1, u2);
-		assertEquals("tx更新用户出错!", true, result2);
+		boolean result = userService.NonTransactionUpdate(u1, u2);
+		assertEquals("tx更新用户出错!", true, result);
 	}
 
+	/**
+	 * 模拟controller调service层业务方法事务处理:
+	 * 
+	 * 在controller层中使用try..catch..处理捕获异常不会影响service层的事务处理
+	 */
+	@Test
+	public void test02(){
+		try {
+			UserService userService = (UserService) ctx.getBean("userService");
+			User u1 = new User(1,"bo","1111",new Account(1,100));
+			User u2 = new User(2,"lin","1809", new Account(2,100));
+			boolean result = userService.txUpdateUser(u1, u2);
+			assertEquals("tx更新用户出错!", true, result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
